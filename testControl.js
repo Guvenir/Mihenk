fb7ed5fb7bb69691b7055b7c0e54ad4aafc4ab7b
Guvenir <omerguvenir@yandex.com>
2016-06-01 21:07:14 +0300

    first

--- testControl.js
+++ testControl.js
@@ -25,9 +25,14 @@ app.controller('d3demo', function($scope) {
         if ($scope.height !== undefined && $scope.height !== "") {
             $scope.grafik.height = $scope.height;
         }
-        console.log($scope.grafik);
+        //console.log($scope.grafik);
         $scope.chart($scope.grafik);
     }
+    
+    $scope.test2 = function(){
+        $scope.chart2($scope.grafik);
+    }
+
     $scope.chart = function(grafik) {
         var xdata = [];
         var ydata = [];
@@ -41,18 +46,13 @@ app.controller('d3demo', function($scope) {
         var mihenkdata = {
             "xdata": [1, 2, 3, 4, 5],
             "ydata": [4, 5, 6, 7, 8],
-            "ranges": [
-                {
-                    "x_labels": ["1", "2", "3", "4", "6", "8"],
-                    "y_labels": []
-                }
-            ],
             "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
                 "xlab": "x label",
                 "ylab": "y label",
-                "color": "",
-                "width": "",
-                "height": "",
+                "color": [],
                 "div_id": "graph"
             }
         };
@@ -60,88 +60,22 @@ app.controller('d3demo', function($scope) {
         mihenkdata.xdata = xdata;
         mihenkdata.ydata = ydata;
 
-        /*
-        var mihenkdata = {
-            "dset": [
-                 {
-                "y": 7,
-                "x": 1
-            },
-            {
-                "y": 10,
-                "x": 2
-            },
-            {
-                "y": 3,
-                "x": 3
-            },
-            {
-                "y": 10,
-                "x": 4
-            },
-            {
-                "y": 1,
-                "x": 5
-            },
-            {
-                "y": 1,
-                "x": 6
-            },
-            {
-                "y": 9,
-                "x": 7
-            },
-            {
-                "y": 1,
-                "x": 8
-            },
-            {
-                "y": 10,
-                "x": 9
-            },
-            {
-                "y": 4,
-                "x": 10
-            }
-            ],
-            "ranges": [
-                {
-                    "x_labels": ["1", "2", "3", "4", "6", "8"],
-                    "y_labels": []
-                }
-            ],
-            "opt": {
-                "xlab": "x label",
-                "ylab": "y label",
-                "color": "",
-                "width": "",
-                "height": "",
-                "div_id":"graph"
-            }
-        };
-        */
-
         var mihenkdata2 = {
             "xdata": [4, 5, 6, 7, 8],
             "ydata": [1, 2, 3, 4, 5],
-            "ranges": [
-                {
-                    "x_labels": ["1", "2", "3", "4", "6", "8"],
-                    "y_labels": []
-                }
-            ],
             "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
                 "xlab": "x label",
                 "ylab": "y label",
-                "color": "",
-                "width": "",
-                "height": "",
+                "color": [],
                 "div_id": "graph2"
             }
         }
 
         var mihenkdata3 = {
-            "dset": [
+            "data": [
                 {
                     "ymin": 10.4,
                     "lower": 14.5,
@@ -240,42 +174,274 @@ app.controller('d3demo', function($scope) {
                     "x": 14
                 }
             ],
-            "ranges": [
-                {
-                    "x.range": [2.4, 14.6],
-                    "x.labels": ["3", "4", "5"],
-                    "x.major": [0.1875, 0.5, 0.8125],
-                    "x.minor": {},
-                    "x.major_source": [1, 2, 3],
-                    "x.minor_source": {},
-                    "y.range": [9.225, 35.075],
-                    "y.labels": ["10", "15", "20", "25", "30", "35"],
-                    "y.major": [0.03, 0.2234, 0.4168, 0.6103, 0.8037, 0.9971],
-                    "y.minor": [0.03, 0.1267, 0.2234, 0.3201, 0.4168, 0.5135, 0.6103, 0.707, 0.8037, 0.9004, 0.9971],
-                    "y.major_source": [10, 15, 20, 25, 30, 35],
-                    "y.minor_source": [10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30, 32.5, 35]
-                }
-            ],
-            "opt":{
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
                 "xlab": "x label",
                 "ylab": "y label",
-                "color": "",
-                "width": "",
-                "height": "",
+                "color": [],
                 "div_id": "graph3"
             }
         }
-        
+
         mihenkdata2.xdata = ydata;
         mihenkdata2.ydata = xdata;
         //console.log(mihenkdata);
+
+        mihenkdata4 = {
+            "data": [
+                {
+                    "y": 16.1067,
+                    "ymin": 14.4004,
+                    "ymax": 17.8129,
+                    "x": 3
+                },
+                {
+                    "y": 24.5333,
+                    "ymin": 21.5478,
+                    "ymax": 27.5189,
+                    "x": 4
+                },
+                {
+                    "y": 21.38,
+                    "ymin": 15.5433,
+                    "ymax": 27.2167,
+                    "x": 5
+                }
+            ],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph4"
+            }
+        }
+
+        mihenkdata5 = {
+            "xdata": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            "ydata": [21, 18, 23, 16, 25, 20, 27, 35, 29, 18, 16, 26, 33, 19, 35],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph5"
+            }
+        }
 
         mihenk.barchart(mihenkdata);
         mihenk.barhorizontal(mihenkdata2);
         //barchart(dset, ranges, grafik);
 
         mihenk.box(mihenkdata3);
+        mihenk.error(mihenkdata4);
+        mihenk.line(mihenkdata5);
 
+    }
+
+    $scope.chart2 = function(grafik) {
+        var xdata = [];
+        var ydata = [];
+        var tempa = [];
+        a = Math.floor((Math.random() * 100) + 1);
+        for (t = 0; t < 25; t++) {
+            xdata.push(a + t);
+            ydata.push(Math.floor((Math.random() * 100) + 1));
+        }
+
+        var mihenkdata = {
+            "xdata": [1, 2, 3, 4, 5],
+            "ydata": [4, 5, 6, 7, 8],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph"
+            }
+        };
+        mihenkdata.xdata = xdata;
+        mihenkdata.ydata = ydata;
+        mihenk.barchart(mihenkdata);
+
+    }
+    
+    $scope.barhorizontal = function(){
+        var xdata = [];
+        var ydata = [];
+        var tempa = [];
+        a = Math.floor((Math.random() * 100) + 1);
+        for (t = 0; t < 25; t++) {
+            xdata.push(a + t);
+            ydata.push(Math.floor((Math.random() * 100) + 1));
+        }
+
+        var mihenkdata = {
+            "xdata": [1, 2, 3, 4, 5],
+            "ydata": [4, 5, 6, 7, 8],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph"
+            }
+        };
+
+        //mihenkdata.xdata = ydata;
+        //mihenkdata.ydata = xdata;
+        mihenk.barhorizontal(mihenkdata);
+    };
+    
+    $scope.barvertical = function(){
+        var xdata = [];
+        var ydata = [];
+        var tempa = [];
+        a = Math.floor((Math.random() * 100) + 1);
+        for (t = 0; t < 25; t++) {
+            xdata.push(a + t);
+            ydata.push(Math.floor((Math.random() * 100) + 1));
+        }
+
+        var mihenkdata = {
+            "xdata": [1, 2, 3, 4, 5],
+            "ydata": [4, 5, 6, 7, 8],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph"
+            }
+        };
+
+        //mihenkdata.xdata = xdata;
+        //mihenkdata.ydata = ydata;
+        mihenk.barchart(mihenkdata);
+    };
+    
+    $scope.box = function(){
+        var mihenkdata = {
+            "data": [
+                {
+                    "ymin": 10.4,
+                    "lower": 14.5,
+                    "middle": 15.5,
+                    "upper": 18.4,
+                    "ymax": 21.5,
+                    "x": 3
+                },
+                {
+                    "ymin": 17.8,
+                    "lower": 21,
+                    "middle": 22.8,
+                    "upper": 28.075,
+                    "ymax": 33.9,
+                    "x": 4
+                },
+                {
+                    "ymin": 15,
+                    "lower": 15.8,
+                    "middle": 19.7,
+                    "upper": 26,
+                    "ymax": 30.4,
+                    "x": 5
+                },
+                {
+                    "ymin": 10.4,
+                    "lower": 14.5,
+                    "middle": 15.5,
+                    "upper": 18.4,
+                    "ymax": 21.5,
+                    "x": 6
+                },
+                {
+                    "ymin": 17.8,
+                    "lower": 21,
+                    "middle": 22.8,
+                    "upper": 28.075,
+                    "ymax": 33.9,
+                    "x": 7
+                }
+            ],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph3"
+            }
+        }
+        
+        mihenk.box(mihenkdata);
+    }
+    
+    $scope.error = function(){
+        var mihenkdata = {
+            "data": [
+                {
+                    "y": 16.1067,
+                    "ymin": 14.4004,
+                    "ymax": 17.8129,
+                    "x": 3
+                },
+                {
+                    "y": 24.5333,
+                    "ymin": 21.5478,
+                    "ymax": 27.5189,
+                    "x": 4
+                },
+                {
+                    "y": 21.38,
+                    "ymin": 15.5433,
+                    "ymax": 27.2167,
+                    "x": 5
+                }
+            ],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph4"
+            }
+        }
+        
+        mihenk.error(mihenkdata);
+    }
+    
+    $scope.line = function(){
+        var mihenkdata = {
+            "xdata": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            "ydata": [21, 18, 23, 16, 25, 20, 27, 35, 29, 18, 16, 26, 33, 19, 35],
+            "opt": {
+                "x_labels": [],
+                "y_labels": [],
+                "zoom": true,
+                "xlab": "x label",
+                "ylab": "y label",
+                "color": [],
+                "div_id": "graph5"
+            }
+        }
+        
+        mihenk.line(mihenkdata);
     }
 
     $scope.svgindir = function(val) {
